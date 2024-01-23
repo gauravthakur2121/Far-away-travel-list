ecc0a95ad8d9a6ffe9fb4e285a97a7cabf8a0489
gauravthakur2121 <shishodiag12@gmail.com>
2024-01-23 14:04:52 +0530

    change the form component and reused

--- src/PackingList.js
+++ src/PackingList.js
@@ -2,24 +2,38 @@
 
 const PackingList = () => {
   const initialItems = [
-    { id: 1, description: "Passports", quantity: 2, packed: false },
-    { id: 2, description: "Socks", quantity: 12, packed: false },
+    { id: 1, description: "Passports", quantity: 2, packed: true },
+    { id: 2, description: "Socks", quantity: 12, packed: true },
+    { id: 3, description: "Charger", quantity: 1, packed: false },
   ];
   return (
   
-      <ul className="list">
+      <div className="list">
+        <ul>
         {initialItems.map((item)=> (
-          <Item item={item}/>
+          <Item item={item}  key={item.id}/>
 
         ))}
+
+        </ul>
+        </div>
       
-      </ul>
+      
   
   );
-}
+        }
  
 function Item({item}){
-  return <li>{item.description}</li>;
+  return (
+  <li>
+    <span style={item.packed ? {textDecoration: "line-through" }: {}}>
+    {item.quantity} {item.description}
+    </span>
+    <button>❌</button>
+    
+    </li>
+  )
+  
 
 
 }
